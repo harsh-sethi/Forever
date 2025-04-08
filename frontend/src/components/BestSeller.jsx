40f45524d1dab6d59e7e0067dec624ca49acbfc5
Harsh Sethi <harshsethi135@gmail.com>
2025-04-08 13:21:23 +0530

    second commit

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -15,7 +15,7 @@ const BestSeller = () => {
       <div className="text-center text-3xl py-8">
         <Title text1={"BEST"} text2={"SELLERS"} />
         <p className="w-3/4 m-auto text-xs sm:text:sm md:text-base text-gray-600">
-          Discover our most-loved styles that customers can’t get enough of!
+          Discover our most-loved styles that customers can't get enough of!
         </p>
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
